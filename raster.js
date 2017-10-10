22c266c243d2ec4c99c05b5c1d5e5e005ad3e84e
Michael Davis <rubiksmaster65@yahoo.com>
2017-10-10 15:52:42 -0400

    Write up a function based on atomic object article
    Article by Eric Shull:
    https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/

--- raster.js
+++ raster.js
@@ -1,12 +1,48 @@
 (function (global) {
 
     global.rasterize = function (element) {
+        setHeaders(element);
+        let str = getOuterHTML(element);
+        let xml = prependXML(str);
+        let image = saveImage(xml);
+        image.onload = function () {
+            let canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            let context = canvas.getContext('2d');
+            context.drawImage(image, 0, 0);
 
+            let a = document.createElement('a');
+            a.download = 'image.png';
+            a.href = canvas.toDataURL('image/png');
+            document.body.appendChild(a);
+            a.click();
+    }
+
+    function setHeaders(el) {
+        el.setAttribute('version', '1.1');
+        el.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        el.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
     }
 
     function getOuterHTML(el) {
         let outer = document.createElement('div');
         outer.appendChild(el.cloneNode(true));
         return outer.innerHTML;
+    }
+
+    function getStyles(dom) {
+        //I inline all styles, but this method should be written
+        //eventually
+    }
+
+    function prependXML(string) {
+        return "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">;\n" + string;
+    }
+
+    function saveImage(xml) {
+        let image = new Image();
+        image.src = 'data:image/svg+xml;base64,' + global.btoa(unescape(encodeURIComponent(xml)));
+        return image;
     }
 })(window);
