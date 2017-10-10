11803564a4c0a383debae990df2844a9eabb7138
Michael Davis <rubiksmaster65@yahoo.com>
2017-10-10 13:41:31 -0400

    Add method to get outer HTML

--- raster.js
+++ raster.js
@@ -1,3 +1,12 @@
-function rasterize (element) {
-    
-}
+(function (global) {
+
+    global.rasterize = function (element) {
+
+    }
+
+    function getOuterHTML(el) {
+        let outer = document.createElement('div');
+        outer.appendChild(el.cloneNode(true));
+        return outer.innerHTML;
+    }
+})(window);
